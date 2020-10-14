8817725c60a5224a4f0362aff0074bdb633f1747
kartiknawal <kartiknawal.7@gmail.com>
2020-10-14 09:25:33 +0530

    [kartik] Add. functionality to validate first name

--- UserRegistrationProgram/Program.cs
+++ UserRegistrationProgram/Program.cs
@@ -7,6 +7,10 @@ namespace UserRegistrationProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Regex User Registration Problem");
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter the first name");
+            string fname = Console.ReadLine();
+            pattern.validateFirstName(fname);
         }
     }
 }
