262189650b249ff097e4872aeb9574c4e01eda19
kartiknawal <kartiknawal.7@gmail.com>
2020-10-15 08:05:49 +0530

    [kartik] Add. functionality to validate Email

--- UserRegistrationProgram/Program.cs
+++ UserRegistrationProgram/Program.cs
@@ -14,6 +14,9 @@ namespace UserRegistrationProgram
             Console.WriteLine("Enter the Last name");
             string lastName = Console.ReadLine();
             pattern.validateName(lastName);
+            Console.WriteLine("Enter the the email id");
+            string email = Console.ReadLine();
+            pattern.validateEmail(email);
         }
     }
 }
